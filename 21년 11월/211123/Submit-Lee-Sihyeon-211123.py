51ad1f854b24b9fd56809ead01dd094f09f651db
LeeSihyeon <shyeon0528@gmail.com>
2021-11-23 11:21:06 +0900

    9020_골드바흐의 추측

---
+++ 21년 11월/211123/Submit-Lee-Sihyeon-211123.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+3
+8
+10
+16
+
+T = int(input())
+primeArray = [0 for _ in range(10001)]
+primeArray[0], primeArray[1] = 1,1
+for i in range(2,100):
+    for j in range(i*i, 10001, i): # i*i 부터 시작해서 i만큼 더하면서 체크
+        primeArray[j] = 1
+
+
+for _ in range(T):
+    n = int(input())
+    s = n//2
+    for i in range(s):
+        
+        if primeArray[s-i] ==0 and primeArray[s+i] == 0:
+            print(s-i, s+i)
+            break
+
